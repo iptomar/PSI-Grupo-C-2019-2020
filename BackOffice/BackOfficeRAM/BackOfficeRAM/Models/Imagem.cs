a10caa7989cc8ef6395506f4765a120d54bce67a
Gonçalo Ferreira <aluno18137@ipt.pt>
2020-03-10 21:43:54 +0000

    Criação das tabelas da base de dados

---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/Models/Imagem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOfficeRAM.Models
+{
+    public class Imagem
+    {
+        public String ConteudoImagem { get; set; }
+    }
+}
---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/Models/PontoInteresse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOfficeRAM.Models
+{
+    public class PontoInteresse
+    {
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public int MyProperty { get; set; }
+        public List<Coordenada> Coordenadas { get; set; }
+        public List<Imagem> Imagens { get; set; }
+    }
+}
