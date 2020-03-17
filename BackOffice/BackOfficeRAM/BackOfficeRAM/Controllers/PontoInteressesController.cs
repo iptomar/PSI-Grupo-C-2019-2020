a41be43186fe6068dda9b1bc841b7270bdeb3009
Gonçalo Ferreira <aluno18137@ipt.pt>
2020-03-17 21:38:43 +0000

    Correção controllers e views

--- BackOffice/BackOfficeRAM/BackOfficeRAM/Controllers/PontoInteressesController.cs
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/Controllers/PontoInteressesController.cs
@@ -46,7 +46,7 @@ namespace BackOfficeRAM.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Nome,Descricao,MyProperty")] PontoInteresse pontoInteresse)
+        public ActionResult Create([Bind(Include = "Id,Nome,Descricao")] PontoInteresse pontoInteresse)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace BackOfficeRAM.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nome,Descricao,MyProperty")] PontoInteresse pontoInteresse)
+        public ActionResult Edit([Bind(Include = "Id,Nome,Descricao")] PontoInteresse pontoInteresse)
         {
             if (ModelState.IsValid)
             {
