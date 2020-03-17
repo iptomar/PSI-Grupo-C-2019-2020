c6d7d3f6a1ab45d88a73df70afed8a719833aaf0
Gonçalo Ferreira <aluno18137@ipt.pt>
2020-03-17 20:36:31 +0000

    controler imagens

---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/Controllers/ImagemsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BackOfficeRAM.Models;
+
+namespace BackOfficeRAM.Controllers
+{
+    public class ImagemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Imagems
+        public ActionResult Index()
+        {
+            return View(db.Imagens.ToList());
+        }
+
+        // GET: Imagems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Imagem imagem = db.Imagens.Find(id);
+            if (imagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(imagem);
+        }
+
+        // GET: Imagems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Imagems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ConteudoImagem")] Imagem imagem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Imagens.Add(imagem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(imagem);
+        }
+
+        // GET: Imagems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Imagem imagem = db.Imagens.Find(id);
+            if (imagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(imagem);
+        }
+
+        // POST: Imagems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ConteudoImagem")] Imagem imagem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(imagem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(imagem);
+        }
+
+        // GET: Imagems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Imagem imagem = db.Imagens.Find(id);
+            if (imagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(imagem);
+        }
+
+        // POST: Imagems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Imagem imagem = db.Imagens.Find(id);
+            db.Imagens.Remove(imagem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- BackOffice/BackOfficeRAM/BackOfficeRAM/Migrations/Models/Database/PontoInteresse.cs
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/Migrations/Models/Database/PontoInteresse.cs
@@ -12,7 +12,6 @@ namespace BackOfficeRAM.Models
         public int Id { get; set; }
         public String Nome { get; set; }
         public String Descricao { get; set; }
-        public int MyProperty { get; set; }
         public List<Coordenada> Coordenadas { get; set; }
         public List<Imagem> Imagens { get; set; }
     }
