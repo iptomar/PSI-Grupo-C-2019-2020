76661370d2793deaeadd8882e1cc1fe5c56d86cc
goncalo-ferreira <37107783+goncalo-ferreira@users.noreply.github.com>
2020-03-20 00:34:05 +0000

    Revert "Remoçao dos ficheiros para novo upload"
    This reverts commit fc5b4d8618fdc18b9ee47e34490ee99d13921db7.

---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/Controllers/PontoInteressesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BackOfficeRAM.Models;
+
+namespace BackOfficeRAM.Controllers
+{
+    public class PontoInteressesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: PontoInteresses
+        public ActionResult Index()
+        {
+            return View(db.PontosInteresse.ToList());
+        }
+
+
+
+        // GET: PontoInteresses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PontoInteresse pontoInteresse = db.PontosInteresse.Find(id);
+            if (pontoInteresse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pontoInteresse);
+        }
+
+        // GET: PontoInteresses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PontoInteresses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Descricao")] PontoInteresse pontoInteresse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PontosInteresse.Add(pontoInteresse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pontoInteresse);
+        }
+
+        // GET: PontoInteresses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PontoInteresse pontoInteresse = db.PontosInteresse.Find(id);
+            if (pontoInteresse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pontoInteresse);
+        }
+
+        // POST: PontoInteresses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Descricao")] PontoInteresse pontoInteresse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pontoInteresse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pontoInteresse);
+        }
+
+        // GET: PontoInteresses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PontoInteresse pontoInteresse = db.PontosInteresse.Find(id);
+            if (pontoInteresse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pontoInteresse);
+        }
+
+        // POST: PontoInteresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PontoInteresse pontoInteresse = db.PontosInteresse.Find(id);
+            db.PontosInteresse.Remove(pontoInteresse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/Models/Database/PontoInteresse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BackOfficeRAM.Models
+{
+    public class PontoInteresse
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public List<Coordenada> Coordenadas { get; set; }
+        public List<Imagem> Imagens { get; set; }
+    }
+}
