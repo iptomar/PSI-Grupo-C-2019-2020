7fb98781cf3fa51d34371ba2d0fd1d38cd5ecc6d
Frederico Riachos <aluno20460@ipt.pt>
2020-03-24 20:40:22 +0000

    Primeiros testes

---
+++ SeleniumTest/venv/Test1/UnitTest1.py
@@ -0,0 +1,25 @@
+import unittest
+
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+
+class PythonGoogleSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
+
+    def test_search_in_python_google(self):
+        driver = self.driver
+        driver.get("http://google.com")
+        self.assertIn("Google", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("Automation Step By Step")
+        elem.send_keys(Keys.RETURN)
+        assert "No result found" not in driver.page_source
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
