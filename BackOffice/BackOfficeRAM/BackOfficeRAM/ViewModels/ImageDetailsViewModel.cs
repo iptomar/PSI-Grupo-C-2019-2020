98ff267f673c0f6bf34d4049cafdd336583f6c5e
goncalo-ferreira <37107783+goncalo-ferreira@users.noreply.github.com>
2020-03-27 01:04:51 +0000

    Relacionamento das imagens com os pontos

---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/ViewModels/ImageDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BackOfficeRAM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOfficeRAM.ViewModels
+{
+    public class ImageDetailsViewModel
+    {
+        public Imagem Imagem { get; set; }
+        public PontoInteresse Ponto { get; set; }
+    }
+}
