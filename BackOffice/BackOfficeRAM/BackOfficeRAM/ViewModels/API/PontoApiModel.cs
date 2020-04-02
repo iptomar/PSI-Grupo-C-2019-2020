856113ba501f2fc51e693411052cd63b70379a00
goncalo-ferreira <37107783+goncalo-ferreira@users.noreply.github.com>
2020-04-02 17:59:24 +0100

    adição de API

---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/ViewModels/API/PontoApiModel.cs
@@ -0,0 +1,22 @@
+﻿using BackOfficeRAM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackOfficeRAM.ViewModels.API
+{
+    public class PontoApiModel 
+    {
+        public PontoApiModel(PontoInteresse ponto)
+        {
+            Id = ponto.Id;
+            Nome = ponto.Nome;
+            TipoEdificio = ponto.TipoEdificio;
+        }
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String TipoEdificio { get; set; }
+    }
+}
