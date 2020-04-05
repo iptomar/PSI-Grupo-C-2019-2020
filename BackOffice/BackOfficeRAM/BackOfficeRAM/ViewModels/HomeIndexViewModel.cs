a8078d99d30a4224b4d2769cb1c2b4d519f3c72a
goncalo-ferreira <37107783+goncalo-ferreira@users.noreply.github.com>
2020-04-05 22:03:32 +0100

    alteraçoes interface

---
+++ BackOffice/BackOfficeRAM/BackOfficeRAM/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOfficeRAM.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public int NumPontos { get; set; }
+        public int NumImagens { get; set; }
+
+        public HomeIndexViewModel()
+        {
+            
+        }
+    }
+}
