14a771eacc1bf451025d1e6f94dd748d5bd8c5fa
Frederico Riachos <aluno20460@ipt.pt>
2020-04-28 16:38:04 +0100

    Testes do Download APP e Roteiros com o tratar de exepções e mensagens de feedback

---
+++ venv/Test1/Test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+drive = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
+
+drive.set_page_load_timeout(10)
+drive.get("https://ramtomar.azurewebsites.net")
+drive.find_element_by_id("exampleInputEmail").send_keys("wdq", Keys.TAB, "dwdaf", Keys.ENTER)
+
+#drive.find_element_by_name("q").send_keys(Keys.ENTER)
+time.sleep(4)
+drive.quit()
---
+++ venv/Test1/UnitTest2.py
@@ -0,0 +1,43 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Ram(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r"C:\Users\night\PycharmProjects\selenium\venv\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe")
+    def test_search_LogIN(self):
+        driver = self.driver
+        driver.get("https://ramtomar.azurewebsites.net/")
+        self.assertIn("RAM Tomar Login", driver.title)
+        driver.find_element_by_id("exampleInputEmail").send_keys("a", (Keys.TAB), "a", (Keys.ENTER))
+        try:
+            assert "Invalid login attempt." not in driver.page_source
+            print(f"\nTest LogIn OK!")
+        except Exception as err:
+            print (f"\nTest LogIn Failure!!!")
+    def test_search_DownloadAPP(self):
+        driver = self.driver
+        driver.get("https://ramtomar.azurewebsites.net/")
+        self.assertIn("RAM Tomar Login", driver.title)
+        driver.find_element_by_id("exampleInputEmail").send_keys("a", (Keys.TAB), "a", (Keys.ENTER))
+        assert "Invalid login attempt." not in driver.page_source
+        driver.set_page_load_timeout(10)
+        driver.find_element_by_xpath("//div[@class='card sale-diffrence-border']").click()
+        try:
+            driver.find_element_by_class_name("AHFaub")
+            print (f"\nTest DownloadAPP OK!")
+        except Exception as err:
+            print (f"\nTest DownloadAPP Failure!!!")
+    def test_search_Roteiros(self):
+        driver = self.driver
+        driver.get("https://ramtomar.azurewebsites.net/")
+        self.assertIn("RAM Tomar Login", driver.title)
+        driver.find_element_by_id("exampleInputEmail").send_keys("a", (Keys.TAB), "a", (Keys.ENTER))
+        assert "Invalid login attempt." not in driver.page_source
+        driver.set_page_load_timeout(10)
+        driver.find_element_by_xpath("//div[@class='card card-border-amarelo']").click()
+        try:
+            driver.find_element_by_xpath("//a[@class='btn btn-primary']")
+            print(f"\nTest Roteiros OK!")
+        except Exception as err:
+            print(f"\nTest Roteiros Failure!!!")
