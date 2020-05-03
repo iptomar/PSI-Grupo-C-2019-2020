bd6211531793285f5cecef654b91371246437ecc
Velez <aluno18367@ipt.pt>
2020-05-03 18:56:41 +0100

    2ª alteração para buscar os pontos na api

--- App Ram Tomar/Guia/platforms/browser/www/js/index.js
+++ App Ram Tomar/Guia/platforms/browser/www/js/index.js
@@ -1,13 +1,77 @@
-async function getPontos() {
-  // Begin accessing JSON data here 
-  const list = await fetch("https://backofficeram.azurewebsites.net/api/pontosapi", {mode: 'no-cors'})
-  .then(
-	x => x.json() 
-	)
-  .then(
-	x => {}
-	);
+ //criar o mapa atraves da biblioteca da leaflet com a posiçao definida e zoom
+//O maxZoom quando inicializa a aplicaão é 14 e vai localizar o longitude e latitude na posicão atual 
+//do utilizador
+var mymap = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 14});
 
-  console.log(list);
-  
+//Apenas existe o estado online pois ao consumir a API o estado offline deixa de fazer sentido
+document.addEventListener("online", onOnline, false);
+function onOnline() {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        minZoom: 14,
+        attribution: attribution
+    }).addTo(mymap); 
 }
+    onOnline();
+
+//Custom icon for current position
+const markerCurrent = L.icon({
+    iconUrl: 'img/green.png',
+    iconSize: [75, 75],
+    iconAnchor: [25, 16]
+});
+
+
+var marker;
+ //vai indicar a posição inicial quando inicia a app
+ navigator.geolocation.getCurrentPosition(function (location) {
+    var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+    //Criar o marcador do mapa na localizacão atual
+    marker = L.marker(latlng, { icon: markerCurrent}).addTo(mymap);
+    
+    buildings();
+    marker.bindPopup("<b>Sua localização atual</b>").openPopup();
+    
+});
+
+
+//Funcão para fazer fetch das informacões 
+var coord;
+async function API() {
+    let response = await fetch('https://ramtomar.azurewebsites.net/api/pontosapi');
+    let data = await response.json();
+    let dados = data.data; 
+    
+    $.each(dados, function(i, info){
+        coord = [info.coordinate1, info.coordinate2];
+        circle = L.circle(coord, {
+        color: 'green',
+        fillColor: '#aa3',
+        fillOpacity: 0.5,
+        radius: 100
+    }).addTo(mymap);
+
+    L.marker(coord).bindPopup(`
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content" style="height:300px; width:300px;overflow:scroll;">
+                    <div class="modal-header">
+                        <h1 class="modal-title" id="exampleModalLabel">`+info.buildingName+`</h1><hr>
+                        <h2 class="modal-title" id="exampleModalLabel">`+info.buildingType+`</h2>
+                        <h4>Localizacão: `+info.location+`</h4>
+                        <h5>Ano: `+info.dates+`</h5>
+                    </div><hr>
+                    <div class="modal-footer">
+                        <a class+"btn btn-primary" type="button" href="#detalhe">Ver detalhes</a>
+                    </div><br>
+                </div>
+            </div>
+            </div>`).addTo(mymap);
+            
+            $('#detalhe').html(
+                `<div class="modal-body">
+                    <p>`+info.description+`</p>
+                </div>`
+            );
+    });
+} 
--- App Ram Tomar/Guia/www/js/index.js
+++ App Ram Tomar/Guia/www/js/index.js
@@ -1,72 +1,171 @@
-//Vai buscar a div do mapa criada no index.html
-var map = document.getElementById('mapid');
-//criar o mapa atraves da biblioteca da leaflet com as coordenadas iniciais do utilizador
-var RAMmap = L.map('mapid').locate({ setView: true, maxZoom: 14 });
+var container = document.getElementById('container');
 
-// Devido ao consumo da API o estado offline da app foi removido
-document.addEventListener("online", onOnline, true);
-function onOnline() {
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-        minZoom: 14,
-        attribution: attribution
-    }).addTo(mymap);
-}
-onOnline();
+var app = {
+    // Application Constructor
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
 
-//Custom icon for current position
-const markerCurrent = L.icon({
-    iconUrl: 'img/green.png',
-    iconSize: [75, 75],
-    iconAnchor: [25, 16]
-});
+    },
 
-var marker;
-var circle;
-//coloca o mapa na localização do utilizador
-navigator.geolocation.getCurrentPosition(function (location) {
-    var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-    //Criar o marcador na localização do utilizador
-    marker = L.marker(latlng, { icon: markerCurrent }).addTo(mymap);
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
 
-    //Fetch dos pontos de intresse colocados na API
-    fetch('https://ramtomar.azurewebsites.net/api/pontosapi')
-        .then((res) => { return res.json() })
-        .then((data) => {
-            let result = data.data;                        
-            result.forEach((data) => {
-                circle = L.circle([data.coordinate1, data.coordinate2], {
-                    color: 'green',
-                    fillColor: '#aa3',
-                    fillOpacity: 0.5,
-                    radius: 100
-                }).addTo(RAMmap);
+    onDeviceReady: function () {
+            var topo = document.getElementById('topo');
+            var body = document.body;
+            body.classList.add('overflow');
 
-                L.marker([data.coordinate1, data.coordinate2]).bindPopup(`
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content pontoInter" style="height:300px; width:300px;overflow:scroll;">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title" id="exampleModalLabel">`+ data.buildingName + `</h1><hr>
-                                        <h2 class="modal-title" id="exampleModalLabel">`+ data.buildingType + `</h2>
-                                        <h4>Localizacão: `+ data.location + `</h4>
-                                        <h5>Ano: `+ data.dates + `</h5>
-                                    </div><hr>
-                                    <div class="modal-body">
-                                        <p>`+ data.description + `</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-info btn-lg" onclick="PDI()">Ver detalhes</button>
-                                    </div><br>
-                                </div>
-                            </div>
-                            </div>`).addTo(RAMmap);
+            //buscar o div criado no html
+            var mapa = document.getElementById('mapid');
+            //criar o mapa atraves da biblioteca da leaflet com a posiçao definida e zoom
+            var mymap = L.map('mapid').setView([39.60360511, -8.40795278], 16);
+            //.locate({setView: true, maxZoom: 16});
+            var estado = 0;
+
+            //Como o consumo da API não pode funcionar em Offline, deixamos apenas o modo online
+            document.addEventListener("online", onOnline, false);
+
+            function onOnline() {
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(mymap);
+            }
+                onOnline();
+                
+                //Custom icon for current position
+                const markerCurrent = L.icon({
+                    iconUrl: 'img/green.png',
+                    iconSize: [25, 25],
+                    iconAnchor: [25, 16]
+                });
+
+                var marker;
+                //posição inicial
+                navigator.geolocation.getCurrentPosition(function (location) {
+                   var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+                   //marcador na posição inicial
+                   marker = L.marker(latlng, { icon: markerCurrent}).addTo(mymap);
+
+                   marker.bindPopup("<b>Sua posição atual</b>").openPopup();     
+               });
+
+
+                var jsonData;
+                var control;
+                //buscas do elementos criados no html
+                var btPos = document.getElementById('btPosicao');
+                var icon = document.getElementById('idIcon');
+                var divInfo = document.getElementById('infoAdicional');
+                var divFullImg = document.getElementById('fullImg');
+
+            //Funcão para fazer fetch das cordenadas dos edificios
+            let geojson;
+            $.getJSON('https://ramtomar.azurewebsites.net/api/pontosapi', function(data) {
+              geojson = data;
+              const dataPoint = geojson.datapoint;
+            
+              // store all the coordinates in this array o be able to iterate over the markers array
+              let coordinates = [];
+            
+              // populate coordinates array with all the markers
+              for (let i = 0; i < dataPoint.length; i++) {
+                coordinates.push([Number(dataPoint[i].lat1), Number(dataPoint[i].long1)], [Number(dataPoint[i].lat2), Number(dataPoint[i].long2)]);
+              };
+            
+              // visualize the markers on the map
+              for (let i = 0; i < coordinates.length; i++) {
+                L.marker(coordinates[i]).addTo(map)
+                  .bindPopup("<b>Latitude:</b> " + coordinates[i][0] + " <b>Longitude:</b> " + coordinates[i][1]);
+              };
             });
 
-        });
-    marker.bindPopup("<b>You are here now!</b>").openPopup();
-});
+            var br = document.createElement('br');
 
-function PDI(){
-    document.getElementById("myModal");
-}
+            //função para abrir a imagem no ecra inteiro com opção de zoom
+            function ecraImagem(imgP) {
+
+                var topo = document.getElementById('topo');
+                topo.classList.add('hidden');
+                divInfo.classList.add("hidden");
+
+                divFullImg.classList.remove('hidden');
+                //é criado um canvas com a ajuda do ficheiro img-touch-canvas.js usar touch gestures no dispositivo
+                var gesturableImg = new ImgTouchCanvas({
+                    canvas: document.getElementById('mycanvas'),
+                    path: "" + imgP + "",
+                });
+                document.body.style.background = "#000000";
+            }
+
+
+            /* Busca o div do Acerca e o Buttão do sobre e faz o onclick*/
+            var divAcerca = document.getElementById('idAcerca');
+            var btSobre = document.getElementById('idSobreBt');
+            btSobre.onclick = mostraSobre => {
+                divAcerca.classList.remove('hidden');
+                mapa.classList.add('hidden');
+                divInfo.classList.add('hidden');
+                btPos.classList.add('hidden');
+                body.classList.remove('overflow');
+            }
+            /****************************************************************/
+
+            var idP = document.createElement('p');
+            divInfo.appendChild(idP);
+
+            //trabalha o evento do botao dos dispositivos para voltar atras
+            document.addEventListener("backbutton", onBackKeyDown, false);
+            function onBackKeyDown(e) {
+
+                //sair da aplicação quando esta está no mapa
+                if (mapa.classList.contains('hidden') === false) {
+
+                    navigator.app.exitApp();
+
+                    /* *************** para sair da imagem ***************  */
+                } else if (topo.classList.contains('hidden') === true) {
+
+                    window.scrollTo(0, 0);
+                    document.body.style.background = "#F2F2F2";
+                    topo.classList.remove('hidden');
+                    divInfo.classList.remove('hidden');
+                    divFullImg.classList.add('hidden');
+
+                    /******************************************************* */
+                    /* *********** sair do Sobre para o mapa  *************  */
+                } else if (divAcerca.classList.contains('hidden') === false) {
+                    // imgFull.innerHTML = "";
+
+                    divInfo.innerHTML = "";
+                    e.preventDefault();
+                    window.scrollTo(0, 0);
+                    btPos.classList.remove('hidden');
+                    icon.classList.remove('hidden');
+                    mapa.classList.remove('hidden');
+                    divInfo.classList.add('hidden');
+                    divAcerca.classList.add('hidden');
+                    body.classList.add('overflow');
+                    mymap.closePopup();
+                    /******************************************************* */
+                } else {
+                    /* ***** sair da informação do edificio para o mapa **** */
+                    //imgFull.innerHTML = "";
+
+                    divInfo.innerHTML = "";
+                    e.preventDefault();
+                    window.scrollTo(0, 0);
+                    btPos.classList.remove('hidden');
+                    icon.classList.remove('hidden');
+                    mapa.classList.remove('hidden');
+                    divInfo.classList.add('hidden');
+                    body.classList.add('overflow');
+                    mymap.closePopup();
+                    /******************************************************* */
+                }
+            }
+        }
+};
+
+app.initialize();
